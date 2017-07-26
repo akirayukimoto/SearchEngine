f4c961f443712a7176d96ce6c988311c8b8762dd
sun549 <sun549@purdue.edu>
2017-07-26 13:48:38 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -323,9 +323,10 @@ SearchEngine::dispatch( FILE * fout, const char * documentRequested)
 
 
   for ( int i = 0; i < count; i++ ) {
-  	if (llist[i] == NULL) continue;
+  	if (llist[i] != NULL) {
     fprintf( fout, "<h3>%d. <a href=\"%s\">%s</a><h3>\n", counter + 1, llist[i]->_url, llist[i]->_url);
     fprintf( fout, "<blockquote>%s<p></blockquote>\n", llist[i]->_description);
+    }
     	counter++;
 	
   }
